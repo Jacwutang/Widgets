acfe46842517036b494ba2e8dce1f8d8eea3b2c1
Jack Wu <jackwu350@yahoo.com>
2018-10-29 22:56:29 -0700

    implement name matcher

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import Weather from './weather';
-import Clock from './clock';
-import Tabs from './tabs';
-import AutoComplete from './autocomplete';
-import './stylesheets/index.css';
+import React, { Component } from "react";
+import "./App.css";
+import Clock from "./clock";
+import Tabs from "./tabs";
+import AutoComplete from "./autocomplete";
+import "./stylesheets/index.css";
 
 class App extends Component {
-
   render() {
     return (
       <div>
-        <AutoComplete
-        names=
-        {
-          [
-            "John",
-            "Joe",
-            "Abby",
-            "Anna",
-            "Zyzzbra"
-          ]
-        }
-        />
+        <AutoComplete names={["John", "Joe", "Abby", "Anna", "Zyzzbra"]} />
         <Clock />
         <Tabs
-        headers = {
-        [
-        {"title": "Tab 1", "content": "Hello World"},
-        {"title": "Tab 2", "content": "Hello World Part 2"}
-        ]}
+          headers={[
+            { title: "Tab 1", content: "Hello World" },
+            { title: "Tab 2", content: "Hello World Part 2" }
+          ]}
         />
       </div>
     );
--- src/autocomplete/index.js
+++ src/autocomplete/index.js
@@ -1,49 +1,42 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class AutoComplete extends Component{
-  constructor(props){
+class AutoComplete extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      query: "",
-    }
+      query: ""
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState( {query: event.currentTarget.value} );
+    this.setState({ query: event.currentTarget.value });
   }
 
   matches() {
-    let results = this.props.names.map(
+    let filtered = this.props.names.filter(
+      name =>
+        name.toLowerCase().slice(0, this.state.query.length) ===
+        this.state.query.toLowerCase()
+    );
 
-      (name,idx) =>
-        <li
-        key={idx}>
-        {name}
-        </li>
-      )
-      return results;
+    let results = filtered.map((name, idx) => <li key={idx}>{name}</li>);
+    return results;
   }
 
   render() {
-
-    const {names} = this.props;
-    return(
+    const { names } = this.props;
+    return (
       <div className="auto">
-        <ul>
-          {
-            this.matches()
-          }
-        </ul>
+        <ul>{this.matches()}</ul>
 
         <input
-          style={{backgroundColor: "white", color: "black"}}
+          style={{ backgroundColor: "white", color: "black" }}
           value={this.state.query}
-          placeholder='Search...'
-          onChange={this.handleChange} />
-
+          placeholder="Search..."
+          onChange={this.handleChange}
+        />
       </div>
-
     );
   }
 }
